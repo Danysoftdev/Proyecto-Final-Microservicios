cc45346fa1d7671aebb3e210c833654e8bbb0ff0
Danysoftdev <danysoftdev@gmail.com>
2024-10-12 00:24:40 -0500

    Created settings file

---
+++ FastAPI/app/config/settings.py
@@ -0,0 +1,16 @@
+"""
+This file contains the database settings for the application.
+"""
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE = {
+    "name": os.getenv("MYSQL_DATABASE"),
+    "engine": "peewee.MySQLDatabase",
+    "user": os.getenv("MYSQL_USER"),
+    "password": os.getenv("MYSQL_PASSWORD"),
+    "host": os.getenv("MYSQL_HOST"),
+    "port": int(os.getenv("MYSQL_PORT")),
+}
